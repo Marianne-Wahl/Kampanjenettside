3712ce8292bd7c211fdcd1c854b2f7127763446e
Marianne-Wahl <marianne.gabrielsen@gmail.com>
2024-10-29 19:17:16 +0100

    Added day 13 files

--- hamburgermeny.js
+++ hamburgermeny.js
@@ -1,16 +1,17 @@
+// Konstante elementer / elementer som ikke endrer seg, henter style fra CSS
 const Hamburgermeny = document.querySelector ('.Hamburgermeny');
 const NavigasjonsbarOverlay = document.querySelector ('.NavigasjonsbarOverlay');
 const CloseButton = document.querySelector('.close-btn');
 
 
-// Open overlay when hamburger menu is clicked
+// Åpner overlay når hamburgermenyen blir trykket på
 Hamburgermeny.addEventListener('click', () => {
-    NavigasjonsbarOverlay.classList.add('active');  // Show overlay by adding 'active' class
-    Hamburgermeny.classList.add('active');          // Indicate that hamburger menu is active
+    NavigasjonsbarOverlay.classList.add('active');  // Viser overlay ved bruk av 'active' class
+    Hamburgermeny.classList.add('active');          // Indikerer at hamburgermenyen er aktiv
 });
 
-// Close overlay when close button is clicked
+// Lukker overlay når X blir trykket på
 CloseButton.addEventListener('click', () => {
-    NavigasjonsbarOverlay.classList.remove('active'); // Hide overlay by removing 'active' class
-    Hamburgermeny.classList.remove('active');         // Reset hamburger menu state
+    NavigasjonsbarOverlay.classList.remove('active'); // Gjemmer overlay ved å fjerne 'active' class
+    Hamburgermeny.classList.remove('active');         // Resetter hamburgermenyens aktiv
 });
